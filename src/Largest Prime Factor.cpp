96388617bbce8b74f307fb60b7c12ebf164223ea
Bhashkar-Pandit <36663417+Bhashkar-Pandit@users.noreply.github.com>
2018-02-21 01:06:07 +0530

    Euler Problem 003 added

---
+++ src/Largest Prime Factor.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+int maxp(int n)
+{
+    int maxPrime = -1;
+    while (n % 2 == 0) {
+        maxPrime = 2;
+        n /= 2;
+    }
+    for (int i = 3; i <= sqrt(n); i += 2) {
+        while (n % i == 0) {
+            maxPrime = i;
+            n = n / i;
+        }
+    }
+    if (n > 2)
+        maxPrime = n;
+    return maxPrime;
+}
+
+signed main()
+{
+    int test;
+    cin>>test;
+    while(test--)
+    {
+        int n;
+        cin>>n;
+        int ans=maxp(n);
+        cout<<ans<<"\n";
+        
+    }
+    return 0;
+}
